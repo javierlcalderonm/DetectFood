3cbee85833b7baad5b5dc7247c03cc7a51b7ade5
Javier Calderon <javier.calderon@globant.com>
2017-12-15 18:52:48 -0500

    Detect Hotdog!

--- DetectFood/ViewController.swift
+++ DetectFood/ViewController.swift
@@ -28,13 +28,52 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
         present(imagePicker, animated: true, completion: nil)
     }
     
+    func detect(ciimage: CIImage)
+    {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loadinfg CoreML Model Failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+            //print(results)
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog")
+                {
+                    self.navigationItem.title = "Is a Hot Dog!"
+                }else{
+                    self.navigationItem.title = firstResult.identifier
+                }
+                print(String(firstResult.identifier) + " - " + String(firstResult.confidence))
+            }
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: ciimage)
+        
+        do {
+            try handler.perform([request])
+        }catch{
+            print(error)
+        }
+    }
+    
     // Picker Delegate
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let userPickedimage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageViewPhoto.image = userPickedimage
+            guard let ciimage = CIImage(image:userPickedimage) else {
+                fatalError("Could not convert UIImage inti UIImage")
+            }
+            
+            detect(ciimage: ciimage)
         }
         imagePicker.dismiss(animated: true, completion: nil)
     }
+    
 }
 
